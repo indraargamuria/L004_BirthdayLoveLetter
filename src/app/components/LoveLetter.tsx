5071977f3f932db896b273aff2bc4d86e1257804
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-14 23:40:53 +0700

    Love letter modification 3

--- src/app/components/LoveLetter.tsx
+++ src/app/components/LoveLetter.tsx
@@ -7,7 +7,10 @@ const LoveLetter = () => {
           Halo wanita cantikku, Nilam Mayawati Po Wijaya
         </p>
         <p className="text-lg text-gray-700 mb-6">
-          Hari ini kamu bertambah dewasa
+          Hari ini kamu bertambah usia dan menjadi semakin dewasa
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          Pertama-tama, aku ingin mengapresiasi betapa hebatnya dirimu dalam menjalani peran sebagai istriku, juga bunda dari anak kita
         </p>
         <p className="text-xl font-semibold text-gray-800">Dengan cinta (dan malam), Suamimu 💌</p>
       </div>
