e2d48c0c61e4ea2f635e15ad5c03193922ab4906
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-14 23:39:27 +0700

    Love letter modification

--- src/app/components/LoveLetter.tsx
+++ src/app/components/LoveLetter.tsx
@@ -4,7 +4,7 @@ const LoveLetter = () => {
       <div className="bg-pink-100 p-8 rounded-lg shadow-lg text-center max-w-2xl mx-auto mt-8">
         <h2 className="text-3xl text-gray-700 font-bold mb-4">Selamat ulang tahun yang ke-27</h2>
         <p className="text-lg text-gray-700 mb-6">
-          Kamu cantik!
+          Halo wanita cantikku, Nilam Mayawati Po Wijaya 
         </p>
         <p className="text-xl font-semibold text-gray-800">Dengan cinta (dan malam), Suamimu 💌</p>
       </div>
